b261f36cf496ab2e59c84576b3806d77337ac42c
AyushMishra98 <146470718+AyushMishra98@users.noreply.github.com>
2023-10-02 17:55:06 +0530

    Time: 129 ms (16.67%), Space: 85.4 MB (16.67%) - LeetHub

--- 2870-minimum-number-of-operations-to-make-array-empty/2870-minimum-number-of-operations-to-make-array-empty.cpp
+++ 2870-minimum-number-of-operations-to-make-array-empty/2870-minimum-number-of-operations-to-make-array-empty.cpp
@@ -1,29 +1,18 @@
 class Solution {
 public:
-    int calmin(int n,vector<int> &dp){
-        if(n==0)
-            return 0;
-        if(n<0)
-            return 1000000;
-        if(dp[n] == -1){
-            return dp[n]= 1+min(calmin(n-3,dp),calmin(n-2,dp));
-        }
-        return dp[n];
-    }
     int minOperations(vector<int>& nums) {
         unordered_map<int,int> m;
         for(auto x:nums)
             m[x]++;
-        int maxi=INT_MIN;
-        for(auto x:m)
-            maxi=max(maxi,x.second);
-        vector<int> dp(maxi+1,-1);
         int res=0;
         for(auto x:m){
-            int temp=calmin(x.second,dp);
-            if(temp >= 1000000)
+            if(x.second == 1)
                 return -1;
-            res+=temp;
+            int val=x.second/3;
+            int rem=x.second%3;
+            if(rem >0)
+                val++;
+            res+=val;
         }
         return res;
     }
