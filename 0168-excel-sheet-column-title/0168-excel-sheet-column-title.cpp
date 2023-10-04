5de24ceaddd4822e6cc6deaab8c2852d454dfdfb
AyushMishra98 <146470718+AyushMishra98@users.noreply.github.com>
2023-10-05 00:54:12 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (18.39%) - LeetHub

---
+++ 0168-excel-sheet-column-title/0168-excel-sheet-column-title.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string convertToTitle(int n) {
+        if(n<27){
+            string str="";
+            str+=char(n+'A'-1);
+            return str;
+        }
+        string c = "";
+        while (n > 0){
+            if(n%26 == 0){
+                c+='A'+25;
+                n-=1;
+            }else{
+                c+=(n%26+'A'-1);
+            }
+            n/=26;
+        }
+        reverse(c.begin(), c.end());
+        return c;
+    }
+};
