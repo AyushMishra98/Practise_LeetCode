4ac8ac4f989994bad0241cb8d9d379f5bd399806
AyushMishra98 <146470718+AyushMishra98@users.noreply.github.com>
2023-10-04 09:39:06 +0530

    Time: 99 ms (86.19%), Space: 53.7 MB (63.39%) - LeetHub

---
+++ 0706-design-hashmap/0706-design-hashmap.cpp
@@ -0,0 +1,49 @@
+class MyHashMap {
+public:
+    vector<list<pair<int,int>>> m;
+    int sz;
+    MyHashMap() {
+        sz=1000;
+        m.resize(sz);
+    }
+    int hash(int key){
+        return key%sz;//returning index
+    }
+    list<pair<int,int>> :: iterator search(int key){
+        int i=hash(key);
+        auto it=m[i].begin();
+        while(it!=m[i].end()){
+            if(it->first == key)
+                return it;
+            it++;
+        }
+        return it;
+    }
+    void put(int key, int value) {
+        remove(key);
+        int i=hash(key);
+        m[i].push_back({key,value});
+    }
+    int get(int key) {
+        int i=hash(key);
+        auto itr=search(key);
+        if(itr != m[i].end())
+            return itr->second;
+        return -1;
+    }
+    
+    void remove(int key) {
+        int i=hash(key);
+        auto itr=search(key);
+        if(itr != m[i].end())
+            m[i].erase(itr);
+    }
+};
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap* obj = new MyHashMap();
+ * obj->put(key,value);
+ * int param_2 = obj->get(key);
+ * obj->remove(key);
+ */
