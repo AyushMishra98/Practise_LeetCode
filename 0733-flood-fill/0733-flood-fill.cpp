3b60179345b4bcac4ebf3819e002c15920c766d2
AyushMishra98 <146470718+AyushMishra98@users.noreply.github.com>
2023-12-25 10:06:52 +0530

    Time: 8 ms (53.04%), Space: 14.2 MB (82.59%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int x[8]={-1,0,1,0};
     int y[8]={0,1,0,-1};
-    void DFS(vector<vector<int>>& img,int i,int j,int val){
+    void DFS(vector<vector<int>>& img,int i,int j,int color,int val){
         if(i<0 || i>=img.size())
             return;
         if(j<0 || j>=img[0].size())
@@ -10,16 +10,15 @@ public:
         
         if(img[i][j] != val)
             return;
-        img[i][j]=-1;
+        
+        img[i][j]=color;
+        
         for(int k=0;k<4;k++)
-            DFS(img,i+x[k],j+y[k],val);
+            DFS(img,i+x[k],j+y[k],color,val);
     }
     vector<vector<int>> floodFill(vector<vector<int>>& img, int sr, int sc, int color) {
-        DFS(img,sr,sc,img[sr][sc]);
-        for(int i=0;i<img.size();i++)
-            for(int j=0;j<img[i].size();j++)
-                if(img[i][j] == -1)
-                    img[i][j]=color;
+        if(img[sr][sc] != color)
+            DFS(img,sr,sc,color,img[sr][sc]);
         return img;
     }
 };
