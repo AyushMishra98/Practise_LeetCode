e5428155ad2f328631d99cee26c48bae17deaa61
AyushMishra98 <146470718+AyushMishra98@users.noreply.github.com>
2023-12-25 19:00:06 +0530

    Time: 3 ms (47.64%), Space: 9.1 MB (7.51%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -12,20 +12,17 @@ public:
     int countPatterns(string s,int idx){
         if(idx >= s.length())
             return 1;
+        if(s[idx] == '0')
+            return 0;
         if(dp[idx] == -1){
             dp[idx]=0;
-            if(s[idx] != '0'){
-                dp[idx]+=countPatterns(s,idx+1);
-                if(check(s,idx))
-                dp[idx]+=countPatterns(s,idx+2);
-            }
+            dp[idx]+=countPatterns(s,idx+1);
+            if(check(s,idx))
+            dp[idx]+=countPatterns(s,idx+2);
         }
         return dp[idx];
     }
     int numDecodings(string s) {
-        for(int i=0;i<s.length()-1;i++)
-            if(s[i] == '0' && s[i+1] == '0')
-                return 0;
         dp.resize(s.length(),-1);
         return countPatterns(s,0);
     }
