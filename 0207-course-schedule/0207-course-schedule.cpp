2a4a891f120689d7149c05c62a7cf497b6bfa040
AyushMishra98 <146470718+AyushMishra98@users.noreply.github.com>
2023-12-26 09:41:36 +0530

    Time: 17 ms (66.62%), Space: 13.1 MB (99.31%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,33 +1,34 @@
 class Solution {
 public:
-    bool  isCycle(vector<int> adj[],int v){
-        int indgre[v];
-        memset(indgre,0,sizeof(indgre));
-        for(int i=0;i<v;i++)
-            for(auto x:adj[i])
-                indgre[x]++;
+    //Based on kahn's BFS based algorithm
+bool  isCycle(vector<int> adj[],int v){
+    int indgre[v];
+    memset(indgre,0,sizeof(indgre));
+    for(int i=0;i<v;i++)
+        for(auto x:adj[i])
+            indgre[x]++;
 
-        queue<int> q;
-        for(int i=0;i<v;i++)
-            if(indgre[i] == 0)
-                q.push(i);
-        int count=0;
-        while(!q.empty()){
-            int temp=q.front();
-            q.pop();
-            for(auto x:adj[temp]){
-                indgre[x]--;
-                if(indgre[x] == 0)
-                    q.push(x);
-            }
-            count++;
+    queue<int> q;
+    for(int i=0;i<v;i++)
+        if(indgre[i] == 0)
+            q.push(i);
+    int count=0;
+    while(!q.empty()){
+        int temp=q.front();
+        q.pop();
+        for(auto x:adj[temp]){
+            indgre[x]--;
+            if(indgre[x] == 0)
+                q.push(x);
         }
-        return (count!= v);
+        count++;
     }
-    bool canFinish(int n, vector<vector<int>>& p) {
-        vector<int> adj[n];
+    return (count!= v);
+}
+    bool canFinish(int numCourses, vector<vector<int>>& p) {
+        vector<int> adj[numCourses];
         for(int i=0;i<p.size();i++)
             adj[p[i][1]].push_back(p[i][0]);
-        return !isCycle(adj,n);
+        return !isCycle(adj,numCourses);
     }
 };
