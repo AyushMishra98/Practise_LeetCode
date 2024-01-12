03bfe9c156b9d43f758948363e3b13b2f5367927
AyushMishra98 <146470718+AyushMishra98@users.noreply.github.com>
2024-01-12 09:32:13 +0530

    Time: 3 ms (33.32%), Space: 6.6 MB (71.95%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,20 +1,23 @@
 class Solution {
 public:
-    //Tabulation or Bottom up
+    //Optimal solution
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m,vector<int> (n,0));
+        vector<int> prev(n,0);
         
-        dp[0][0]=1;
         for(int i=0;i<m;i++){
+            vector<int> temp(n,0);
+            
             for(int j=0;j<n;j++){
+                
                 if(i == 0 && j == 0)
-                    continue;
+                    temp[j]=1;
                 if(i > 0)
-                    dp[i][j]+=dp[i-1][j];
+                    temp[j]+=prev[j];
                 if(j > 0)
-                    dp[i][j]+=dp[i][j-1];
+                    temp[j]+=temp[j-1];
             }
+            prev=temp;
         }
-        return dp[m-1][n-1];
+        return prev[n-1];
     }
 };
