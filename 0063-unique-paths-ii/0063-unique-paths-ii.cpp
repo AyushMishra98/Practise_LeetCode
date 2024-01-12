a88754b6213ed0e1b2c2ffc708b5f9fd21f114d4
AyushMishra98 <146470718+AyushMishra98@users.noreply.github.com>
2024-01-12 10:11:27 +0530

    Time: 5 ms (40.36%), Space: 8.2 MB (48.35%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -20,7 +20,22 @@ public:
         if(grid[0][0] == 1 || grid[m-1][n-1] == 1)
             return 0;
         
-        vector<vector<int>> dp(m,vector<int> (n,-1));
-        return count(grid,m-1,n-1,dp);
+        vector<vector<int>> dp(m,vector<int> (n,0));
+        
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0)
+                    dp[i][j]=1;
+                
+                if(grid[i][j] == 1)
+                    continue;
+                
+                if(i>0)
+                    dp[i][j]+=dp[i-1][j];
+                if(j>0)
+                    dp[i][j]+=dp[i][j-1];
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
