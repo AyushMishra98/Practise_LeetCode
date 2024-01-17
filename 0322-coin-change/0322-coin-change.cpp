1693bef2222c21acfdd26f2bbc5c4993c02a3d79
AyushMishra98 <146470718+AyushMishra98@users.noreply.github.com>
2024-01-17 09:11:27 +0530

    Time: 75 ms (56.89%), Space: 19 MB (36.40%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -2,24 +2,25 @@ class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
         int n=coins.size();
-        vector<vector<int>> dp(n,vector<int> (amount+1,0));
+        vector<int> prev(amount+1,0),curr(amount+1,0);
         
         for(int amt=0;amt<=amount;amt++)
             if(amt%coins[0] == 0)
-                dp[0][amt]=amt/coins[0];
+                prev[amt]=amt/coins[0];
             else
-                dp[0][amt]=1e9;
+                prev[amt]=1e9;
         for(int idx=1;idx<n;idx++){
             for(int amt=0;amt<=amount;amt++){
-            int notTake=dp[idx-1][amt];
+            int notTake=prev[amt];
             
             int take=INT_MAX;
             if(coins[idx]<=amt)
-                take=1+dp[idx][amt-coins[idx]];
+                take=1+curr[amt-coins[idx]];
             
-            dp[idx][amt]=min(take,notTake);
+            curr[amt]=min(take,notTake);
             }
+            prev=curr;
         }        
-        return (dp[n-1][amount] >= 1e9)?-1:dp[n-1][amount];
+        return (prev[amount] >= 1e9)?-1:prev[amount];
     }
 };
