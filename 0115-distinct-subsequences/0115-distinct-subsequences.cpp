f3bae24efffc4a9a868cf04319570b84a4b03c34
AyushMishra98 <146470718+AyushMishra98@users.noreply.github.com>
2024-01-18 10:52:49 +0530

    Time: 1683 ms (10.09%), Space: 422.8 MB (6.46%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     int count(int i,int j,string s, string t,vector<vector<int>>& dp){
-        if(j<0)
+        if(j == 0)
             return 1;
-        if(i<0)
+        if(i == 0)
             return 0;
         
         if(dp[i][j] == -1){
-            if(s[i] == t[j])
+            if(s[i-1] == t[j-1])
                 dp[i][j]=count(i-1,j-1,s,t,dp)+count(i-1,j,s,t,dp);
         else
                 dp[i][j]=count(i-1,j,s,t,dp);
@@ -16,7 +16,7 @@ public:
     }
     int numDistinct(string s, string t) {
         int n=s.length(),m=t.length();
-        vector<vector<int>> dp(n,vector<int> (m,-1));
-        return count(n-1,m-1,s,t,dp);
+        vector<vector<int>> dp(n+1,vector<int> (m+1,-1));
+        return count(n,m,s,t,dp);
     }
 };
