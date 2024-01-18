9dc5bda852c476744a3448d99e72617f1c1b3948
AyushMishra98 <146470718+AyushMishra98@users.noreply.github.com>
2024-01-18 16:23:54 +0530

    Time: 49 ms (17.49%), Space: 81.5 MB (10.59%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     int minCount(int i,int j,string w1, string w2,vector<vector<int>>& dp){
-        if(i<0)
-            return j+1;
-        if(j<0)
-            return i+1;
+        if(i == 0)
+            return j;
+        if(j == 0)
+            return i;
         
         if(dp[i][j] == -1){
-            if(w1[i] == w2[j])
+            if(w1[i-1] == w2[j-1])
                 dp[i][j]=minCount(i-1,j-1,w1,w2,dp);
             else{
                 int insert=minCount(i,j-1,w1,w2,dp);
@@ -20,8 +20,8 @@ public:
         return dp[i][j]; 
     }
     int minDistance(string w1, string w2) {
-        int l1=w1.length(),l2=w2.length();
-        vector<vector<int>> dp(l1,vector<int> (l2,-1));
-        return minCount(l1-1,l2-1,w1,w2,dp);
+        int m=w1.length(),n=w2.length();
+        vector<vector<int>> dp(m+1,vector<int> (n+1,-1));
+        return minCount(m,n,w1,w2,dp);
     }
 };
