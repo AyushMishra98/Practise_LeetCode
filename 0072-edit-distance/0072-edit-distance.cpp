f066a5b93e029c4ae3f2c720ab7ea344e81d5db3
AyushMishra98 <146470718+AyushMishra98@users.noreply.github.com>
2024-01-18 16:28:09 +0530

    Time: 13 ms (49.54%), Space: 9.3 MB (48.07%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,27 +1,28 @@
 class Solution {
 public:
-    int minCount(int i,int j,string w1, string w2,vector<vector<int>>& dp){
-        if(i == 0)
-            return j;
-        if(j == 0)
-            return i;
-        
-        if(dp[i][j] == -1){
-            if(w1[i-1] == w2[j-1])
-                dp[i][j]=minCount(i-1,j-1,w1,w2,dp);
-            else{
-                int insert=minCount(i,j-1,w1,w2,dp);
-                int replace=minCount(i-1,j-1,w1,w2,dp);
-                int remove=minCount(i-1,j,w1,w2,dp);
-                
-                dp[i][j]=1+min(insert,min(replace,remove));
-            }
-        }
-        return dp[i][j]; 
-    }
     int minDistance(string w1, string w2) {
         int m=w1.length(),n=w2.length();
-        vector<vector<int>> dp(m+1,vector<int> (n+1,-1));
-        return minCount(m,n,w1,w2,dp);
+        vector<vector<int>> dp(m+1,vector<int> (n+1,0));
+        for(int i=0;i<=m;i++)
+            dp[i][0]=i;
+        
+        for(int j=0;j<=n;j++)
+            dp[0][j]=j;
+        
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                
+                if(w1[i-1] == w2[j-1])
+                    dp[i][j]=dp[i-1][j-1];
+                else{
+                    int insert=dp[i][j-1];
+                    int replace=dp[i-1][j-1];
+                    int remove=dp[i-1][j];
+                
+                    dp[i][j]=1+min(insert,min(replace,remove));
+            }
+            }
+        }
+        return dp[m][n];
     }
 };
