5d691bd30ed9af140415f81c56e2e256107f6fd9
AyushMishra98 <146470718+AyushMishra98@users.noreply.github.com>
2024-01-19 09:58:29 +0530

    Time: 11 ms (80.66%), Space: 11 MB (61.52%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -6,30 +6,32 @@ public:
             return matrix[0][0];
         
         int ans=INT_MAX;
-        vector<vector<int>> dp(m,vector<int>(m,-1));
-
+        
+        vector<int> prev(m);
         for(int i=m-1;i>=0;i--){
+            vector<int> temp(m);
             for(int j=m-1;j>=0;j--){
 
                 if(i == m-1){
-                    dp[i][j]=matrix[i][j];
+                    temp[j]=matrix[i][j];
                     continue;
                 }
 
                 int mini=INT_MAX;
                 if(j-1>=0)
-                    mini=min(mini,dp[i+1][j-1]);
+                    mini=min(mini,prev[j-1]);
 
-                mini=min(mini,dp[i+1][j]);
+                mini=min(mini,prev[j]);
 
                 if(j+1 <m)
-                    mini=min(mini,dp[i+1][j+1]);
+                    mini=min(mini,prev[j+1]);
 
-                dp[i][j]= matrix[i][j]+mini;
+                temp[j]= matrix[i][j]+mini;
 
                 if(i == 0)
-                    ans=min(ans,dp[i][j]);
+                    ans=min(ans,temp[j]);
             }
+            prev=temp;
         }
         return ans;
     }
