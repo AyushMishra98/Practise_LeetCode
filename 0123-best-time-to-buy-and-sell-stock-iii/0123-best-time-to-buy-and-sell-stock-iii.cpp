fe565c9207408b4fbf48acb772e27d0030aae831
AyushMishra98 <146470718+AyushMishra98@users.noreply.github.com>
2024-01-19 18:26:51 +0530

    Time: 107 ms (88.06%), Space: 80.1 MB (68.91%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,38 +1,15 @@
 class Solution {
 public:
-    int maxProf(int idx,bool flag,int cap,vector<int>& p){
-        if(cap == 0)
-            return 0;
-        if(idx == p.size())
-            return 0;
-        int profit=0;
-        if(flag){ 
-            int take=-p[idx]+maxProf(idx+1,0,cap,p);//buying
-            int notTake=maxProf(idx+1,1,cap,p);
-            profit=max(take,notTake);
-        }
-        else{   
-            int take=p[idx]+maxProf(idx+1,1,cap-1,p);//selling 
-            int notTake=maxProf(idx+1,0,cap,p);
-            profit=max(take,notTake);
-        }
-        return profit;
-    }
     int maxProfit(vector<int>& p) {
         int n=p.size();
         int dp[n+1][2][3];
-        for(int i=0;i<2;i++)
-            for(int j=0;j<3;j++)
-                dp[n][i][j]=0;
+        memset(dp,0,sizeof dp);
         
         for(int idx=n-1;idx>=0;idx--){
             for(int flag=0;flag<2;flag++){
                 for(int cap=2;cap>=0;cap--){
-                    if(cap == 0){
-                        dp[idx][flag][0]=0;
+                    if(cap == 0)
                         continue;
-                    }
-                    
             int profit=0;
             if(flag){ 
                 int take=-p[idx]+dp[idx+1][0][cap];//buying
