165b6a76e60e74d56f23eb68d1b385c83184e457
AyushMishra98 <146470718+AyushMishra98@users.noreply.github.com>
2024-01-19 19:33:26 +0530

    Time: 4 ms (57.54%), Space: 13.6 MB (7.70%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -5,19 +5,10 @@ public:
         vector<int> ahead(2,0),after(2,0),curr(2,0);
         
         for(int idx=n-1;idx>=0;idx--){
-            for(int buy=0;buy<2;buy++){
-                int profit=0;
-                if(buy){
-                    int take=-p[idx]+after[0];
-                    int notTake=after[1];
-                    profit=max(take,notTake);
-                }else{
-                    int take=p[idx]+ahead[1];
-                    int notTake=after[0];
-                    profit=max(take,notTake);
-                }
-                curr[buy]=profit;
-            }
+            
+            curr[1]=max(-p[idx]+after[0],after[1]);
+            curr[0]=max(p[idx]+ahead[1],after[0]);
+            
             ahead=after;
             after=curr;
         }
