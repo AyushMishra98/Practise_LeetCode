f0b53b37a9cea25c846e1fa6832fdcebcd74db9d
AyushMishra98 <146470718+AyushMishra98@users.noreply.github.com>
2024-01-23 21:17:02 +0530

    Time: 30 ms (90.63%), Space: 18.6 MB (31.85%) - LeetHub

--- 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.cpp
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.cpp
@@ -1,27 +1,32 @@
 class Solution {
 public:
-    vector<vector<int>> dp;
-    int DFS(vector<vector<int>>& mat,int i,int j,int last){
-        int m=mat.size(), n=mat[0].size();
-        if(i<0 || j<0 || i>=m ||j>=n || mat[i][j] <=last)
-            return 0;
-        if(dp[i][j] != -1)
-            return dp[i][j];
-        int a=DFS(mat,i-1,j,mat[i][j]);
-        int b=DFS(mat,i,j-1,mat[i][j]);
-        int c=DFS(mat,i+1,j,mat[i][j]);
-        int d=DFS(mat,i,j+1,mat[i][j]);
-        int res=max({a,b,c,d})+1;
-        return dp[i][j]=res;
+    int LIP(int i,int j,vector<vector<int>>& mat,vector<vector<int>>& dp){
+        int m=mat.size(),n=mat[0].size();
+        
+        int dx[4]={-1,0,1,0};
+        int dy[4]={0,1,0,-1};
+        
+        if(dp[i][j] == -1){
+            int maxi=1;
+            for(int k=0;k<4;k++){
+                int x=i+dx[k],y=j+dy[k];
+                
+                if(x<0 || x>=m || y<0 || y>=n)
+                    continue;
+                if(mat[i][j] < mat[x][y])
+                    maxi=max(maxi,1+LIP(x,y,mat,dp));
+            }
+            dp[i][j]=maxi;
+        }
+        return dp[i][j];
     }
     int longestIncreasingPath(vector<vector<int>>& mat) {
-        int m=mat.size(), n=mat[0].size();
-        dp.resize(m,vector<int> (n,-1));
-        int res=0;
+        int m=mat.size(),n=mat[0].size();
+        vector<vector<int>> dp(m,vector<int> (n,-1));
+        int maxi=1;
         for(int i=0;i<m;i++)
             for(int j=0;j<n;j++)
-                if(dp[i][j] == -1)
-                    res=max(res,DFS(mat,i,j,-1));
-        return res;
+                maxi=max(maxi,LIP(i,j,mat,dp));
+        return maxi;
     }
 };
