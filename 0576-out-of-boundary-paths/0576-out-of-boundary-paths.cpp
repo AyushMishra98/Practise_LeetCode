e84ee19d9b595145243352e339957da73fd446c5
AyushMishra98 <146470718+AyushMishra98@users.noreply.github.com>
2024-01-26 22:45:39 +0530

    Time: 18 ms (23.69%), Space: 11.1 MB (20.88%) - LeetHub

--- 0576-out-of-boundary-paths/0576-out-of-boundary-paths.cpp
+++ 0576-out-of-boundary-paths/0576-out-of-boundary-paths.cpp
@@ -1,30 +1,34 @@
 class Solution {
 public:
-    int countPaths(int i,int j,int maxMove,int m, int n,vector<vector<vector<int>>>& dp){
-        if(i<0 || i>=m || j<0 ||j>=n)
-            return 1;
-        
-        if(maxMove == 0)
-            return 0;
+    int findPaths(int m, int n, int maxMove, int sR, int sC) {     
+        vector<vector<vector<int>>> dp(maxMove+1,vector<vector<int>> (m+2,vector<int> (n+2,0)));
         
         int mod =1e9+7;
         
         int dx[4]={-1,0,1,0};
         int dy[4]={0,1,0,-1};
         
-        if(dp[i][j][maxMove] == -1){
-            int move=0;
-            for(int k=0;k<4;k++){
-                int x=i+dx[k];
-                int y=j+dy[k];
-                move=(move+countPaths(x,y,maxMove-1,m,n,dp))%mod;
+        for(int mv=0;mv<=maxMove;mv++){
+            for(int i=0;i<m+2;i++){
+                for(int j=0;j<n+2;j++){
+                    if(i==0 || j==0|| i==m+1 || j==n+1)
+                        dp[mv][i][j]=1;
+                }
             }
-            dp[i][j][maxMove]=move;
         }
-        return dp[i][j][maxMove];
-    }
-    int findPaths(int m, int n, int maxMove, int sR, int sC) {      
-        vector<vector<vector<int>>> dp(m,vector<vector<int>> (n,vector<int> (maxMove+1,-1)));
-        return countPaths(sR,sC,maxMove,m,n,dp);
+        for(int mv=1;mv<=maxMove;mv++){
+            for(int i=1;i<m+1;i++){
+                for(int j=1;j<n+1;j++){
+                    int move=0;
+                    for(int k=0;k<4;k++){
+                        int x=i+dx[k];
+                        int y=j+dy[k];
+                        move=(move+dp[mv-1][x][y])%mod;
+                    }
+                    dp[mv][i][j]=move;
+                }
+            }
+        }
+        return  dp[maxMove][sR+1][sC+1];
     }
 };
