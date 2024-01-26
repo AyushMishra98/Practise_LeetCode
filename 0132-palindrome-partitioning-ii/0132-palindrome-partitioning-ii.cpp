a5752554ea41675af4c75b26d52419690ea753ee
AyushMishra98 <146470718+AyushMishra98@users.noreply.github.com>
2024-01-26 10:04:29 +0530

    Time: 1521 ms (27.54%), Space: 7.7 MB (53.75%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -11,18 +11,14 @@ public:
     }
     int minCut(string s) {
         int n=s.length();
-        vector<int> dp(n,-1);
+        vector<int> dp(n+1,0);
         
         for(int i=n-1;i>=0;i--){
             
             int mini=INT_MAX;
             for(int j=i;j<n;j++){
                 if(isPalindrome(i,j,s)){
-                    int cost;
-                    if(j == n-1)
-                        cost=1;
-                    else
-                        cost=1+dp[j+1];
+                    int cost=1+dp[j+1];
                     mini=min(mini,cost);
                 }
             }
