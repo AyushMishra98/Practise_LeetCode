fb89348264c54b35b43eb561826463305391709c
AyushMishra98 <146470718+AyushMishra98@users.noreply.github.com>
2024-01-26 09:58:45 +0530

    Time: 1525 ms (26.72%), Space: 7.8 MB (52.72%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -9,27 +9,25 @@ public:
         }
         return true;
     }
-    int minimumCuts(int i,int n,string& s,vector<int>& dp){
-        if(i == n)
-            return 0;
-        
-        if(dp[i] == -1){
-        
-            int mini=INT_MAX;
-        
-            for(int j=i;j<n;j++){
-                if(isPalindrome(i,j,s)){
-                    int cost=1+minimumCuts(j+1,n,s,dp);
-                    mini=min(mini,cost);
-                }
-            }
-        dp[i]= mini;
-        }
-        return dp[i];
-    }
     int minCut(string s) {
         int n=s.length();
         vector<int> dp(n,-1);
-        return minimumCuts(0,n,s,dp)-1;
+        
+        for(int i=n-1;i>=0;i--){
+            
+            int mini=INT_MAX;
+            for(int j=i;j<n;j++){
+                if(isPalindrome(i,j,s)){
+                    int cost;
+                    if(j == n-1)
+                        cost=1;
+                    else
+                        cost=1+dp[j+1];
+                    mini=min(mini,cost);
+                }
+            }
+            dp[i]=mini;
+        }
+        return dp[0]-1;
     }
 };
