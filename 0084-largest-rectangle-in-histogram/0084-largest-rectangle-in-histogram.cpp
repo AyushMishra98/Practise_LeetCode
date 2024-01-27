083581361b96fb853715fbe127e888ef59b8a7bd
AyushMishra98 <146470718+AyushMishra98@users.noreply.github.com>
2024-01-27 09:02:13 +0530

    Time: 94 ms (98.67%), Space: 77.8 MB (70.40%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,40 +1,26 @@
 class Solution {
 public:
-    vector<int> prevs,nexts;//prev smaller ,next smaller
-    void prevSmaller(vector<int>& nums){
+    int largestRectangleArea(vector<int>& h) {
+        int n=h.size();
         stack<int> s;
-        for(int i=0;i<nums.size();i++){
-            while(!s.empty() && nums[s.top()]>=nums[i])
+        
+        int maxA=0;
+        for(int i=0;i<=n;i++){
+            while(!s.empty() && (i==n || h[s.top()] >= h[i])){
+                
+                int height=h[s.top()];
                 s.pop();
-            if(s.empty())
-                prevs[i]=-1;
-            else
-                prevs[i]=s.top();
+                
+                int width;
+                if(s.empty())
+                    width=i-0;
+                else
+                    width=i-s.top()-1;
+                
+                maxA=max(maxA,height*width);
+            }
             s.push(i);
         }
-    }
-    void nextSmaller(vector<int>& nums){
-        stack<int> s;
-        int n=nums.size();
-        for(int i=n-1;i>=0;i--){
-            while(!s.empty() && nums[s.top()]>=nums[i])
-                s.pop();
-            if(s.empty())
-                nexts[i]=n;
-            else
-                nexts[i]=s.top();
-            s.push(i);
-        }
-    }
-    int largestRectangleArea(vector<int>& heights) {
-        prevs.resize(heights.size());
-        nexts.resize(heights.size());
-        prevSmaller(heights);
-        nextSmaller(heights);
-        int res=0;
-        for(int i=0;i<heights.size();i++){
-            res=max(res,(nexts[i]-prevs[i]-1)*heights[i]);
-        }
-        return res;
+        return maxA;
     }
 };
