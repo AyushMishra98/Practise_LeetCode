82de62a7447c6c3a8de3d79c272a0e7421ec4510
AyushMishra98 <146470718+AyushMishra98@users.noreply.github.com>
2024-02-02 08:19:28 +0530

    Time: 112 ms (83.41%), Space: 105.5 MB (24.93%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -6,18 +6,12 @@ public:
         
         stack<int> s;
         for(int i=0;i<n;i++){
-            if(s.empty())
-                s.push(i);
-            else if(t[s.top()] < t[i]){
-                while(!s.empty() && t[s.top()] < t[i]){
+             while(!s.empty() && t[s.top()] < t[i]){
                     int j=s.top();
                     s.pop();
                     
                     res[j]=i-j;
                 }
-                s.push(i);
-            }
-            else
                 s.push(i);
         }
         return res;
