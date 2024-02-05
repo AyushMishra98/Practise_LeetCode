d37f3f605dcad67085a7b4d9699d67b5a7b61e8c
AyushMishra98 <146470718+AyushMishra98@users.noreply.github.com>
2024-02-05 08:53:06 +0530

    Time: 15 ms (53.18%), Space: 10.3 MB (16.71%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    string minWindow(string s, string t) {                
+        int remain = t.size();
+        //to keep track of frequency of element
+        unordered_map<char, int> m;
+        for(char c : t)
+            m[c]++;
+    
+        int start = 0, len = 0;
+        int left=0,right=0;
+        while(right<s.size()) {
+            char curr = s[right];
+        
+            m[curr]--;
+            if(m.find(curr) != m.end() && m[curr] >= 0)
+                remain--;
+        
+        while(remain == 0) {
+            if(len == 0 || right - left + 1 < len)  {
+                start = left;
+                len = right - left + 1;
+            }
+            
+            curr = s[left];
+            
+            m[curr]++;
+            if(m.find(curr) != m.end() && m[curr] > 0)
+               remain++;
+            
+            left++;
+        }
+        right++;
+    }
+    return s.substr(start, len);
+}
+};
