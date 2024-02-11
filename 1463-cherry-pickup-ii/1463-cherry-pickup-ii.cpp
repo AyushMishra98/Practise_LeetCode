d77dc62c218f3f307339207f7bcc7d281f202efc
AyushMishra98 <146470718+AyushMishra98@users.noreply.github.com>
2024-02-11 23:51:02 +0530

    Time: 78 ms (59.32%), Space: 19.3 MB (24.93%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -17,17 +17,50 @@ public:
         int dx[]={-1,0,1};
         for(int j=0;j<3;j++){
             for(int k=0;k<3;k++){
-                maxi=max(maxi,sum+pickup(i+1,j1+dx[j],j2+dx[k],grid,dp));
+               
             }
         }
-        dp[i][j1][j2]= maxi;
+        
         }
         return dp[i][j1][j2];
     }
     
     int cherryPickup(vector<vector<int>>& grid) {
         int m=grid.size(),n=grid[0].size();
-        vector<vector<vector<int>>> dp(m,vector<vector<int>> (n,vector<int>(n,-1)));
-        return pickup(0,0,grid[0].size()-1,grid,dp);
+        vector<vector<vector<int>>> dp(m,vector<vector<int>> (n,vector<int>(n,0)));
+        
+        for(int i=m-1;i>=0;i--){
+            for(int j1=n-1;j1>=0;j1--){
+                for(int j2=0;j2<n;j2++){
+                    
+                    if(i == m-1){
+                         if(j1 == j2)
+                            dp[i][j1][j2]=grid[i][j1];
+                        else
+                            dp[i][j1][j2]=grid[i][j1]+grid[i][j2];
+                        continue;
+                    }
+                    
+                    int sum;
+                    if(j1 == j2)
+                        sum=grid[i][j1];
+                    else
+                        sum=grid[i][j1]+grid[i][j2];
+                        
+                    int maxi=sum;
+                    int dx[]={-1,0,1};
+                    for(int j=0;j<3;j++)
+                        for(int k=0;k<3;k++){
+                            int x=j1+dx[j];
+                            int y=j2+dx[k];
+                            if(x<0 || x>=n || y<0 || y>=n)
+                                continue;
+                            maxi=max(maxi,sum+dp[i+1][x][y]);
+                        }
+                    dp[i][j1][j2]= maxi;
+                }
+            }
+        }
+        return dp[0][0][n-1];
     }
 };
