784d5fc51849aa5718d78d85086794a902955918
AyushMishra98 <146470718+AyushMishra98@users.noreply.github.com>
2024-03-13 18:45:25 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (73.82%) - LeetHub

--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
@@ -1,10 +1,8 @@
 class Solution {
 public:
     int pivotInteger(int n) {
-        if(n==1)
-            return 1;
-        int low=0,high=n;
-        while(low<high){
+        int low=1,high=n;
+        while(low<=high){
             int mid=low+(high-low)/2;
             int leftSum=(mid*(mid+1))/2;
             int rightSum=((n*(n+1))/2)-leftSum+mid;
