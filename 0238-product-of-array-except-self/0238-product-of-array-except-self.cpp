80eb2e945c1aa2a575805d15629916976e24a1ae
AyushMishra98 <146470718+AyushMishra98@users.noreply.github.com>
2024-03-15 07:26:13 +0530

    Time: 12 ms (83.78%), Space: 26.1 MB (90.93%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -2,15 +2,26 @@ class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
         int n=nums.size();
-        vector<int> res1(n,1),res2(n,1);
-        for(int i=1;i<n;i++){
-            res1[i]=res1[i-1]*nums[i-1];
+        // vector<int> res1(n,1),res2(n,1);
+        // for(int i=1;i<n;i++){
+        //     res1[i]=res1[i-1]*nums[i-1];
+        // }
+        // for(int i=n-2;i>=0;i--){
+        //     res2[i]=res2[i+1]*nums[i+1];
+        // }
+        // for(int i=0;i<n;i++)
+        //     res1[i]=res1[i]*res2[i];
+        // return res1;
+        vector<int> res(n,1);
+        for(int i=1;i<n;i++)
+            res[i]=res[i-1]*nums[i-1];
+        
+        int temp=nums[n-1];
+        for(int i=n-2;i>0;i--){
+            res[i]=res[i]*temp;
+            temp=temp*nums[i];
         }
-        for(int i=n-2;i>=0;i--){
-            res2[i]=res2[i+1]*nums[i+1];
-        }
-        for(int i=0;i<n;i++)
-            res1[i]=res1[i]*res2[i];
-        return res1;
+        res[0]=temp;
+        return res;
     }
 };
