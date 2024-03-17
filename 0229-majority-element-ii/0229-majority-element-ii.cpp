caba98f6c8a81b41d16508d47fdec3997c714810
AyushMishra98 <146470718+AyushMishra98@users.noreply.github.com>
2024-03-17 09:21:00 +0530

    Time: 8 ms (67.03%), Space: 19.5 MB (26.17%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -6,16 +6,25 @@ public:
         unordered_map<int,int> m;
         int n=nums.size();
         
-        for(auto x:nums){
+        //two pass solution
+        for(auto x:nums)
             m[x]++;
+        
+        for(auto x:m)
+            if(x.second > n/3)
+                res.push_back(x.first);
+     
+//         //one pass solution
+//         for(auto x:nums){
+//             m[x]++;
             
-            if(m[x]>n/3 && res.size()<2){
-                if(res.empty())
-                    res.push_back(x);
-                else if(res.back() != x)
-                    res.push_back(x);
-            }
-        }
+//             if(m[x]>n/3 && res.size()<2){
+//                 if(res.empty())
+//                     res.push_back(x);
+//                 else if(res.back() != x)
+//                     res.push_back(x);
+//             }
+//         }
         
         return res;
     }
