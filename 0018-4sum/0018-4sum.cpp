2f068362ef0001916ade2df99c8fc71da9f3f75c
AyushMishra98 <146470718+AyushMishra98@users.noreply.github.com>
2024-03-18 12:56:57 +0530

    Time: 1611 ms (5.00%), Space: 474.1 MB (5.00%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,39 +1,27 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        int n=nums.size();
-        if(n<=3)
-            return {};
-        sort(nums.begin(),nums.end());
-        vector<vector<int>> res;
-        for(int i=0;i<n-3;i++){
-            if(i>0 && nums[i] == nums[i-1])
-                continue;
-            for(int j=i+1;j<n-2;j++){
-                if(j>(i+1) && nums[j] == nums[j-1])
-                    continue;
-                int low=j+1,high=n-1;
-                long long temp_sum=target-((long long)nums[i]+(long long)nums[j]);
-                while(low<high){
-                    long long curr_sum=nums[low]+nums[high];
-                    if(curr_sum == temp_sum){
-                        res.push_back({nums[i],nums[j],nums[low],nums[high]});
-                        while(low<high && nums[low] == nums[low+1]){
-                            low++;
-                        }
-                        while(low<high && nums[high]==nums[high-1]){
-                            high--;
-                        }
-                        low++;
-                        high--;
+        set<vector<int>> store;
+        
+        int  n=nums.size();
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                unordered_set<long long> s;
+                for(int k=j+1;k<n;k++){
+                    long long sum=nums[i]+nums[j];
+                    sum+=nums[k];
+                    
+                    long long element=target-sum;
+                    if(s.find(element) != s.end()){
+                        vector<int> temp={nums[i],nums[j],nums[k],(int)element};
+                        sort(temp.begin(),temp.end());
+                        store.insert(temp);
                     }
-                    else if(curr_sum > temp_sum)
-                        high--;
-                    else
-                        low++;
+                    s.insert(nums[k]);
                 }
             }
         }
-        return res;   
+        vector<vector<int>> res(store.begin(),store.end());
+        return res;
     }
 };
